1e9bbe9a73e3717f740b56dd47a00b03427321b3
janhavigangan13 <janhavigangan07@gmail.com>
2022-07-14 21:52:35 +0530

    first commit

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>    
+<html>    
+<head>    
+    <title>Pet Accessories</title>    
+    <link rel="stylesheet" type="text/css" href="css/style.css">    
+</head>    
+<body> 
+    <img src="https://images.alphacoders.com/107/thumb-1920-1079259.jpg">
+    <div class="login">
+    <h2>Login</h2><br>    
+    <form id="login" method="get" action="index.php">    
+        <label><b>User Name</b>    
+        </label><br>   
+        <input type="text" name="Uname" id="Uname" placeholder="Username">    
+        <br><br>    
+        <label><b>Password</b>    
+        </label><br>
+        <input type="Password" name="Pass" id="Pass" placeholder="Password">    
+        <br><br>    
+        <input type="submit" name="log" id="log" value="Log In">       
+        <br><br>    
+        <input type="checkbox" id="check">    
+        <span>Remember me</span>    
+        <br><br>    
+         Forget Password?<a href="#">Click here</a><br><br>
+         To create an account<a href="register.php">Click here</a>
+    </form>     
+</div>
+</body>
+</html>
